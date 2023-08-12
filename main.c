1218352b02dcd336f9135a2e002cf148aac696c3
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-08-12 07:06:28 +0100

    Add comments to main.c
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

--- main.c
+++ main.c
@@ -5,19 +5,24 @@
  * By: Lewis Evans
  */
 
+/* This code prints "Hello, world!" to the screen, 
+   along with a number and a message at each log level. 
+   This code is intended to be used as a demonstration of 
+   how to use the printc.h header file. */
+
 #include "src/printc.h"
 
 int main()
 {
-    config(true, true, true);
-    printc("Hello, world!\n");
-    int x = 5;
-    printc("Hello, world! %d\n", x);
-    pr_crit("Hello, world!\n");
-    pr_err("Hello, world!\n");
-    pr_warn("Hello, world!\n");
-    pr_info("Hello, world!\n");
-    pr_debug("Hello, world!\n");
-    pr_trace("Hello, world!\n");
+    config(true, true, true); /* Setup the printc library */
+    printc("Hello, world!\n"); /* Print a message */
+    int x = 5; /* Declare a variable */
+    printc("Hello, world! %d\n", x); /* Print a message with a variable */
+    pr_crit("Hello, world!\n"); /* Print a message at the critical log level */
+    pr_err("Hello, world!\n"); /* Print a message at the error log level */
+    pr_warn("Hello, world!\n"); /* Print a message at the warning log level */
+    pr_info("Hello, world!\n"); /* Print a message at the info log level */
+    pr_debug("Hello, world!\n"); /* Print a message at the debug log level */
+    pr_trace("Hello, world!\n"); /* Print a message at the trace log level */
     return 0;
 }
